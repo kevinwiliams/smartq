7d9d606c7caf7ac7f0415313e4a2084e1e6bb265
Kevin Williams <kwilliams9285@gmail.com>
2022-01-22 22:04:32 -0500

    Vendor Files

--- resources/views/backend/common/info.blade.php
+++ resources/views/backend/common/info.blade.php
@@ -1,14 +1,14 @@
 @section('info.message')
     @if(session()->has('message'))
-    <div class="alert alert-success alert-dismissible fade in shadowed alert-dismissible mt-1 mb-1">
+    <div class="alert alert-success alert-dismissible animated--fade-in alert-dismissible mt-1 mb-3">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <p><i class="fa fa-check"></i> {{ session('message') }}</p>
+        <p class="mb-0"><i class="fa fa-check"></i> {{ session('message') }}</p>
     </div>
     @endif 
     @if(session()->has('exception'))
-    <div class="alert alert-success alert-dismissible fade in shadowed alert-dismissible mt-1 mb-1">
+    <div class="alert alert-success alert-dismissible animated--fade-in alert-dismissible mt-1 mb-3">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <p><i class="fa fa-times"></i> {{ session('exception') }}</p>
+        <p class="mb-0"><i class="fa fa-times"></i> {{ session('exception') }}</p>
     </div>
     @endif 
 @stop
@@ -25,55 +25,5 @@
     <span class="label label-primary hidden-xs">v4.1.0</span>
 @stop
 
-@if (strtolower(env('APP_ENV'))=='demo')
-    @section('info.buy-now')
-        <div class="cm-flex"> <a class="btn btn-block btn-lg btn-success" href="https://1.envato.market/ck-queue">Buy now</a></div>
-    @stop
-    
-    @section('info.login-credentials')
-    <div class="col-xs-12" style="margin-top:10px;z-index:999999">
-        <div class="table-responsive mt-1">
-            <table style="cursor:pointer;font-size:12px;z-index:999999" class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>Email</th>
-                        <th>Pass</th> 
-                        <th>Role</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>admin@codekernel.net</td>
-                        <td>12345</td> 
-                        <td>Admin</td> 
-                    </tr>
-                    <tr>
-                        <td>officer@codekernel.net</td>
-                        <td>12345</td> 
-                        <td>Officer</td> 
-                    </tr>
-                    <tr>
-                        <td>receptionist@codekernel.net</td>
-                        <td>12345</td> 
-                        <td>Receptionist</td> 
-                    </tr>  
-                </tbody>
-                <tfoot><tr><th colspan="3"><a class="btn btn-block btn-lg btn-success" href="https://1.envato.market/ck-queue">Buy now</a></th></tr></tfoot>
-            </table> 
-        </div>
-    </div>
 
-    <script type="text/javascript">
-    if(window.self !== window.top | window.location !== window.parent.location) { 
-        var info = document.createElement("h4");
-        info.style.background = "skyblue";
-        info.style.color   = "red";
-        info.style.padding = "10px";
-        info.style.lineHeight = "25px";
-        info.innerHTML = "Demo is not working? browse directly <a target='_blank' href='https://queue.codekernel.net'>https://queue.codekernel.net</a>";                   
-        document.querySelector('.text-center').appendChild(info);
-    }
-    </script>
-    @endsection
-@endif
 
