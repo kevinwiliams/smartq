71bffd884ad021a3cebf6d96889a2f6d752999e1
Kevin Williams <kwilliams9285@gmail.com>
2022-01-27 16:18:30 -0500

    Display icon and link

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -381,27 +381,13 @@
                     <ul class="navbar-nav ml-auto">
 
                         <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ url('common/display?type=4') }}" id="displayLink" 
+                               aria-expanded="false" target="_blank">
+                                <i class="fas fa-desktop fa-fw"></i>
                             </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+                            
+                            
                         </li>
 
                         <!-- Nav Item - Alerts -->
